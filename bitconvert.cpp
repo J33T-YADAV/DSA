8209025f872c9343509abcb421f85c944882011f
J33T-YADAV <your-email@example.com>
2023-09-28 19:50:06 +0530

    new file

---
+++ bitconvert.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int bin(int n){
+    while(n!=0){
+        int bit = n & 1;
+        cout << bit;
+        n = n >> 1;
+    }
+}
+
+int main(){
+    int a;
+    cin>>a;
+    for(int i=1; i<=a; i++){
+        cout << i << " => ";
+        bin(i);
+        cout<<endl;
+    }
+    return 0;
+}
